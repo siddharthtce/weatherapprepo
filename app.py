db64930cb6a06d60df1d44629296cad495f3283a
siddharthtce <siddharthtce@gmail.com>
2023-10-26 09:18:29 +0530

    Weather App commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request , render_template
+import requests # To execute the URL / to get data from URL
+
+app=Flask(__name__)
+
+@app.route('/')
+def homePage():
+    return render_template("index.html")
+
+@app.route('/weatherapp',methods=['POST','GET'])
+def get_weatherdata():
+    url="https://api.openweathermap.org/data/2.5/weather"
+    param={
+        'q':request.form.get('city'),
+        'appid':request.form.get('appid'),
+        'units':request.form.get('units')
+    }
+
+    response=requests.get(url,params=param)
+    data=response.json()
+    return f"data: {data}"
+
+if __name__=='__main__': 
+    app.run(host='0.0.0.0',port=5002) 
+
+
+#2.08.00
+
+# Local -> Git -> Cloud
